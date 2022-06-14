6744a80979cc2d003a9ce4fba2abb8cc4d4273c6
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-14 22:30:18 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,11 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+i = 0
+name_long = ''
+while i < len(names):
+    if len(names[i]) > len(name_long):
+        name_long = names[i]
+    i += 1
+print(name_long)
