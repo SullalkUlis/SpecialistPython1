188b77e0f58e93d4dd52687de8033db67dfa031d
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-15 12:32:39 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,29 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+summa, s = 0, 0
+while True:
+    if text.find(' ', s) - s > 5 or (
+            text.find(' ', s) == -1 and len(text) - s > 5):  # или проверка длины последнего слова
+        summa += 1
+    s = text.find(' ', s) + 1
+    if s == 0:
+        break
+print(summa)
+# ===  2 способ===========================
+summa = 0
+total = 0
+i = 0
+while True:
+    if text[i] != ' ':
+        summa += 1
+    else:
+        if summa > 5:
+            total += 1
+        summa = 0
+    i += 1
+    if i == len(text):
+        if summa > 5:
+            total += 1
+        break
+print(total)
