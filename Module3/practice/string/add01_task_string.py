7db95154fcc08a6f93de926d945ee721b817dafa
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-15 13:28:43 +0300

    Update add01_task_string.py

--- Module3/practice/string/add01_task_string.py
+++ Module3/practice/string/add01_task_string.py
@@ -8,3 +8,17 @@ text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat
 # Примечание: обратите внимание на перенос длинной строки на новую строку
 
 # TODO: your code here
+text_out = ''
+for i in text:
+    if i not in ('.', ',', ':', ';', '!', '"', '"', '?', '\\', '/', '|',):
+        text_out = text_out + i
+summa, s = 0, 0
+while True:
+    if text_out.find(' ', s) - s > 7 or (
+            text_out.find(' ', s) == -1 and len(text_out) - s > 7):  # или проверка длины последнего слова
+        summa += 1
+    s = text_out.find(' ', s) + 1
+    if s == 0:
+        break
+print(summa)
+print(text_out)
