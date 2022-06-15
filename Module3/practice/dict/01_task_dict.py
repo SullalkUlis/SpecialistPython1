7003a7a9f31af6cc7391bd11c8e6d70a54bbf9ed
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-15 18:58:17 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -8,3 +8,22 @@ item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "cou
 dollar_rate = 74.12
 
 # TODO: your code here
+tem = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
+dollar_rate = 74.12
+summa = 0
+rub = 'USD'
+price, quant = 0, 0
+for key, value in item.items():
+    if key == 'price':
+        price = float(value)
+    if key == 'count':
+        quant = int(value)
+    if key == "currency" and value == "rub":
+        rub = 'RUB'
+    elif key == "currency" and value == 'USD':
+        rub = 'USD'
+    if rub == 'USD':
+        summa = summa + price * quant / dollar_rate
+    else:
+        summa = summa + price * quant
+print(round(summa, 2), rub)
