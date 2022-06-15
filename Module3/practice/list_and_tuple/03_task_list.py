8984183a180d543f4f02dab9f672aca58b8b6f46
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-15 13:40:09 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,16 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+import random
+
+n = int(input('Введите количество чисел в списке:'))
+i, summa = 0, 0
+sp_nam = []
+while i < n:
+    sp = random.randint(-10, 10)
+    sp_nam.append(sp)
+    summa += sp
+    print(sp_nam[i], end=' ')
+    i += 1
+print()
+print(summa)
