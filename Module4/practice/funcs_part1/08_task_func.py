bda45fce151c09979f9419708fd9911403e12058
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-20 12:37:10 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,12 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fibonachi(num):
+    fib = 0
+    i, j = 1, 0
+    while i < num:
+        if 0 <= i <= 1:
+            fib = j + i
+        else:
+            fib = j + fib
+            j = fib - j
+        i += 1
+    return fib
