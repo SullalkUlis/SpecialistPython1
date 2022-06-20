26676c9b7054ec82d4976ba931d47b3b13feb4ba
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-20 12:19:46 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,15 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+ef time_f(t):
+    hh = t // 3600
+    mm = t // 60 - hh * 60
+    ss = t % 60
+    if hh < 10:
+        hh = '0' + str(hh)
+    if mm < 10:
+        mm = '0' + str(mm)
+    if ss < 10:
+        ss = '0' + str(ss)
+    str_out = str(hh) + ':' + str(mm) + ':' + str(ss)
+    return str_out
