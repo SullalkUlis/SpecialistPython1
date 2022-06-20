f7019b1598a277382b3e3889cf19c65892be19fa
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-20 11:44:30 +0300

    Update 06_task_func.py

--- Module4/practice/funcs_part1/06_task_func.py
+++ Module4/practice/funcs_part1/06_task_func.py
@@ -3,6 +3,6 @@
 
 def point_in_circle(x, y, xc, yc, r):
     # TODO: your code here
-    pass
+   return  ((x - xc) ** 2 + (y - yc) ** 2) ** 0.5 < r # даже не на самой окружности
 
 # Не забудьте протестировать вашу функцию
