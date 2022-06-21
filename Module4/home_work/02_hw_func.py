dfcaa4fca3cd25a222b68e2603d16acaecd620ca
SullalkUlis <106909971+SullalkUlis@users.noreply.github.com>
2022-06-21 09:07:13 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -4,9 +4,22 @@
 
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
-def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+def distance(p_a, p_b, p_c):
+    xa, ya = p_a
+    xb, yb = p_b
+    xc, yc = p_c
+    len_lin_ab = ((xb - xa) ** 2 + (yb - ya) ** 2) ** 0.5
+    print(len_lin_ab, 'AB')
+    len_lin_ac = ((xc - xa) ** 2 + (yc - ya) ** 2) ** 0.5
+    print(len_lin_ac, 'AC')
+    len_lin_cb = ((xc - xb) ** 2 + (yc - yb) ** 2) ** 0.5
+    print(len_lin_cb, 'CB')
+    if len_lin_ab > len_lin_ac > len_lin_cb:
+        return ' CB'
+    elif len_lin_ab > len_lin_ac:
+        return 'AC'
+    else:
+        return 'AB'
 
 
 # TODO: your code here
